9aa672c31ddb2581d8793ea31f80e6a4e711ec9b
LouisGodfrin <94234242+LouisGodfrin@users.noreply.github.com>
2023-12-02 16:45:31 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,14 @@
-// app.js
 document.getElementById("Year").innerText = new Date().getFullYear();
 
 const javaPackagesApplication = {
     data() {
         return {
             mainHeading: 'Dog breed and information',
-            packages,   // packages: [], 
+            packages,
             isFlipped: false,
             learnMoreUrl: '####',
             userInput: 'Default',
             index: 0,
-            // Image,
         };
     },
 
@@ -19,9 +17,9 @@ const javaPackagesApplication = {
             if (this.packages[this.index + change]) {
                 this.index += change;
                 this.isFlipped = false;
-                // this.Image = Image;
             }
         }
     }
+};
 
 const application = Vue.createApp(javaPackagesApplication).mount("#application");
